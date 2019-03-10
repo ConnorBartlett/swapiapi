fc98d30cddb577700337664de15714b0623f8df1
Connor Bartlett <connorbartlett@Connors-MacBook-Pro.local>
2019-03-10 16:43:29 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  searchForm: FormGroup;
+  submitted = false;
+  baseUrl = 'https://swapi.co/api'
+  _data = {};
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.searchForm = this.formBuilder.group({
+      search: ['']
+    })
+  }
+
+  onSubmit(dropdown, search) {
+    return (this.http.get(`${this.baseUrl}/${dropdown}/?search=${search}`))
+    .subscribe(data => {
+      this._data = data
+    })
+  }
+
   title = 'swapi';
 }
